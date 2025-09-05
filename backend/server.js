d198af845af811b72f6ac6629f8336bd598f1dfb
vagnersantosifpr <161343428+vagnersantosifpr@users.noreply.github.com>
2025-09-05 08:48:27 -0300

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,24 @@ const cors = require('cors');
 
 const app = express();
 
+// Por este bloco de código:
+const whitelist = process.env.WHITELISTED_ORIGINS ? process.env.WHITELISTED_ORIGINS.split(',') : [];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    // A verificação `!origin` permite requisições sem origem, como as de aplicativos mobile ou Postman
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Não permitido pela política de CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
+
 // Middlewares
-app.use(cors()); // Permite requisições de outros domínios (do seu frontend)
+//app.use(cors()); // Permite requisições de outros domínios (do seu frontend)
 app.use(express.json()); // Para o Express entender requisições com corpo em JSON
 app.set('trust proxy', true); // Essencial para obter o IP correto se estiver atrás de um proxy (Heroku, Vercel, etc.)
 
